b4f8c174409ab93c73e62eeb3c722f8387eea6fa
Umar Turdiev <ukcturdiev@icloud.com>
2021-01-14 15:07:14 +0800

    test

---
+++ Checkem.EFDataAccess/DataAccess/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Checkem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkem.EFDataAccess.DataAccess
+{
+    class Class1
+    {
+        Todo todo = new Todo();
+    }
+}
